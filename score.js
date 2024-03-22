d7c1e15be6ff8c56b9cb88fd8055ae702a765c6b
mahimahamsa <160595858+mahimahamsa@users.noreply.github.com>
2024-03-22 11:13:36 +0530

    score.js

--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 5: Store the player score and display it on the game over screen
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const score = localStorage.getItem("score");
+   
+    const scoreElement = document.getElementById("score-board"); 
+    if (scoreElement) {
+        scoreElement.textContent = score; 
+    } else {
+        console.error("Score element not found!"); 
+    }
+});
+
